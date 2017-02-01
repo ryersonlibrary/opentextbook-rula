15440ed28a4472cffa615a2c2d8fceb4c51c2a8c
Patrick Fung <patrick@makestuffdostuff.com>
2017-02-01 11:22:54 -0500

    remove title attribute from a11y toolbar (zoom toggle), and replace with aria-label

---
+++ content-accessibility-toolbar.php
@@ -0,0 +1,5 @@
+<!-- a11y toolbar -->
+<div class="a11y-toolbar">
+  <ul><li><a href="#" aria-label="Toggle font size" role="button" class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize"><span class="dashicons dashicons-visibility"></span></a></li></ul>
+</div>
+<!-- // a11y toolbar -->
